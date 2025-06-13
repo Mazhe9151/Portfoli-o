05813acd2f0e781130f695c098f823b6043f52c8
Mazhe9151 <matthewz61@nycstudents.net>
2025-06-13 15:03:54 +0000

    New project changes, added about me, and attempted to make website all device compatible

--- client/script.js
+++ client/script.js
@@ -1,20 +1,3 @@
-// function updateSizeAndColor() {
-//          const width= window.innerWidth;
-//         const height = window.innerHeight;
-//             // Get window size based on what's inside of the value of the width and height.
-//             let windowWidth = document.getElementById('width');
-//             let windowHeight = document.getElementById('height');
-//             // Update size display based on the text content that was given a new value each time from getElementById
-//             //textContent gets the content of all elements or returns every element in the node
-//             windowWidth.textContent = width; 
-//             windowHeight.textContent = height;
-// }
-            
 
-
-
-
-
-// updateSizeAndColor();
 
 
